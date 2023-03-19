f112ccacf54feca52821a9761d71fe039d37b62e
Zibxto <christianaziba@gmail.com>
2023-03-19 04:15:21 +0100

    added file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+
+	char num = '0';
+
+	while (num < 10)
+	{
+		printf("%i\n", num);
+		num++;
+	}
+
+		return (0);
+}
