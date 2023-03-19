4c7b18fcde1cabe9dc97a997c27906d2018a17d8
Zibxto <christianaziba@gmail.com>
2023-03-19 12:56:11 +0100

    added file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+
+	char num = '0';
+	char alpha = 'a';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	while (alpha <= 'f')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+
+		return (0);
+}
