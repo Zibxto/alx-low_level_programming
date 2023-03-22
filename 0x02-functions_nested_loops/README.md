45f9101d1db4e714ae8281b81a4b61fb84e8b096
Zibxto <christianaziba@gmail.com>
2023-03-22 10:49:08 +0100

    initial commit

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+### Each file contains a program that exemplify use cases of either functions or nested loops
---
+++ 0x02-functions_nested_loops/alxstd.h
@@ -0,0 +1,10 @@
+#ifndef _ALXSTD_H
+#define _ALXSTD_H
+
+/**
+ * _putchar - writes the character c to stdout
+ * Return: On success 1.
+ */
+int _putchar(char c);
+
+#endif
