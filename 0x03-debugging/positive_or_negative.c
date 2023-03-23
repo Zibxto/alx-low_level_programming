e2f801cc8c41164bcac69b1ae6b67f361be7b057
Zibxto <christianaziba@gmail.com>
2023-03-23 10:55:52 +0100

    removed trailing white spaces to comply with betty

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,11 +1,11 @@
 #include "stdio.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
+ * decription - function that prints if integer is positive or negative
+ * @i: integer argument
  * Return: 0
  */
-
-void positive_or_negative(int i) 
+positive_or_negative(int i)
 {
 	if (i < 0)
 	{
