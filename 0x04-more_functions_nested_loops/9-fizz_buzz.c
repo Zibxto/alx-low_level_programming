2d3895657119c7dd279e817fc8efd22496747a36
Zibxto <christianaziba@gmail.com>
2023-03-24 22:29:29 +0100

    updated file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,17 @@ int a;
 for (a = 1; a <= 100; a++)
 {
 
-if (a % 3 == 0)
+if (a % 3 == 0 && a % 5 == 0)
 {
-printf("%s", "Fizz ");
+printf("%s ", "Fizz");
 }
 else if (a % 5 == 0)
 {
-printf("%s", "Buzz ");
+printf("%s ", "Buzz");
 }
-else if (a % 3 == 0 && a % 5 == 0)
+else if (a % 3 == 0)
 {
-printf("%s", "FizzBuzz ");
+printf("%s ", "FizzBuzz");
 }
 else
 {
