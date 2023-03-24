d03650be927e3295244f8a4c7f64e2efa0f5e129
Zibxto <christianaziba@gmail.com>
2023-03-24 12:39:28 +0100

    added file

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+## This directory includ sloutions to more tasks on functions and nested loop
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * _isupper - checks if a letter is uppercase or lowercase
+ * @c: argument to check
+ * Return: 1 if upper and 0 if otherwise
+ */
+int _isupper(int c);
+
+#endif
