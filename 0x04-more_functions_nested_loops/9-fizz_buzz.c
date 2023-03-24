48e24913df17b9ad5232361eef38a539989ea96d
Zibxto <christianaziba@gmail.com>
2023-03-24 22:19:50 +0100

    added file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+int a;
+
+for (a = 1; a <= 100; a++)
+{
+
+if (a % 3 == 0)
+{
+printf("%s", "Fizz ");
+}
+else if (a % 5 == 0)
+{
+printf("%s", "Buzz ");
+}
+else if (a % 3 == 0 && a % 5 == 0)
+{
+printf("%s", "FizzBuzz ");
+}
+else
+{
+printf("%d ", a);
+}
+
+}
+printf("\n");
+return (0);
+}
