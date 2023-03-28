3f78853e53fe3ac0531bde21af49af1fed706c73
Zibxto <christianaziba@gmail.com>
2023-03-28 11:26:33 +0100

    added file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to check
+ * @n: pointer argument
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * reset_to_98 - function to check
+ * @n: pointer argument
+ * Description: takes a pointer to an int as parameter.
+ */
+void reset_to_98(int *n);
+
+#endif
