354a1cffefabaddd1b41d27ffab93b29d2d9e976
Zibxto <christianaziba@gmail.com>
2023-03-28 14:52:32 +0100

    added file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,12 @@
 #define _MAIN_H
 
 /**
+ * _putchar - writes the character c to stdout
+ * Return: On success 1.
+ */
+int _putchar(char c);
+
+/**
  * reset_to_98 - function to check
  * @n: pointer argument
  * Description: takes a pointer to an int as parameter.
@@ -18,13 +24,12 @@ void swap_int(int *a, int *b);
 /**
  * _strlen - returns the length of a string
  * @s: parameter
- * Return: int
  */
 int _strlen(char *s);
 
 /**
  * _puts - prints a string
- * @str: param
+ * @str - param
  * Return: void
  */
 void _puts(char *str);
