e514d14576105712eb6ea8a09f29f563a038e03c
Zibxto <christianaziba@gmail.com>
2023-03-29 16:03:53 +0000

    added file

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+  * _putchar - writes the character c to stdout
+  * @c: param
+  * Return: On success 1.
+  */
+int _putchar(char c);
+
+
+/**
+  * _strcat - function to check
+  * @dest: param
+  * @src: param
+  * Return: char value
+  */
+char *_strcat(char *dest, char *src);
+
+#endif
