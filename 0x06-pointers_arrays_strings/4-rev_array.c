4b500306c6076eea7b0e0e1f10ea9a593fcf47c6
Zibxto <christianaziba@gmail.com>
2023-03-30 11:05:45 +0000

    updated file

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reverse_array - reverse array elements
- * @a: array
- * @n: number of elements in array
+  * reverse_array - reverse array elements
+  * @a: array
+  * @n: number of elements in array
  */
 
 void reverse_array(int *a, int n)
@@ -13,32 +13,8 @@ void reverse_array(int *a, int n)
 	int end = n - 1; /* omit null terminator in length */
 
 	while (beg < end)
-
 	{
-
-
-		tmp = *(a + beg);#include "holberton.h"
-
-			/**
-			 * reverse_array - reverse array elements
-			 * @a: array
-			 * @n: number of elements in array
-			 */
-
-		void reverse_array(int *a, int n)
-		{
-
-			int tmp, beg = 0;
-			int end = n - 1; /* omit null terminator in length */
-
-			while (beg < end)
-			{
-				tmp = *(a + beg);
-				*(a + beg) = *(a + end);
-				*(a + end) = tmp;
-				beg++, end--;
-			}
-		}
+		tmp = *(a + beg);
 		*(a + beg) = *(a + end);
 		*(a + end) = tmp;
 		beg++, end--;
