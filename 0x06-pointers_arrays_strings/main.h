46e30b5da061ceafc071a2c36672f8364a1408f0
Zibxto <christianaziba@gmail.com>
2023-03-30 15:02:44 +0000

    added file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -51,5 +51,10 @@ int _strcmp(char *s1, char *s2);
   */
 void reverse_array(int *a, int n);
 
+/**
+  * string_toupper - function to check
+  * Return: char value
+  */
+char *string_toupper(char *s);
 
 #endif
