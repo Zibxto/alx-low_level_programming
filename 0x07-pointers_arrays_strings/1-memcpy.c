665f42c2414c7e9e932ce65a760c1509643b65f2
Zibxto <christianaziba@gmail.com>
2023-04-04 13:08:03 +0100

    included betty compliance

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,17 +9,19 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	
+
 	if (n > 0)
 	{
 	unsigned int i;
+
 	for (i = 0; i <= n; i++)
 	{
 		dest[i] = src[i];
 	}
 
 	}
-	return(dest);
+
+	return (dest);
 
 }
 
