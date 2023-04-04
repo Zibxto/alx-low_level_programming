3408ac95fe8fabc3a2e65358d1eefddd589d8933
Zibxto <christianaziba@gmail.com>
2023-04-04 22:01:09 +0100

    added file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -66,4 +66,11 @@ char *_strstr(char *haystack, char *needle);
  * Return: void
  */
 void print_chessboard(char (*a)[8]);
+
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square matrix of integers
+ * @a: 2D array
+ * @size: size of array
+ */
+void print_diagsums(int *a, int size);
 #endif
