0780fea0aa6edab2cd713d2cc547622f2777fc86
Zibxto <christianaziba@gmail.com>
2023-04-04 13:04:05 +0100

    added file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: copies to
+ * @src: copies from
+ * @n: memory number of bytes
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	
+	if (n > 0)
+	{
+	unsigned int i;
+	for (i = 0; i <= n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	}
+	return(dest);
+
+}
+
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -17,4 +17,13 @@ int _putchar(char c);
  */
 char *_memset(char *s, char b, unsigned int n);
 
+/**
+ * _memcpy - copies memory area
+ * @dest: copies to
+ * @src: copies from
+ * @n: memory number of bytes
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+
 #endif
