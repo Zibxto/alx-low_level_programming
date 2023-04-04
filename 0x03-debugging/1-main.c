2d6a0409f0668ade9a57b9053fe094385e0e9262
Zibxto <christianaziba@gmail.com>
2023-04-04 12:28:45 +0100

    added file

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
   * main - causes an infinite loop
-  * '\\o/\n"': \o/
   * Return: 0
   */
 
@@ -19,6 +18,6 @@ int main(void)
  *		}
  */
 
-	printf("Infinite loop avoided! \\o/\no");
+	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
