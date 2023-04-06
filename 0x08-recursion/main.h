226f37ddc56486be6b9ce4e040b8c1fc74431550
Zibxto <christianaziba@gmail.com>
2023-04-07 00:46:19 +0100

    added file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -36,4 +36,12 @@ int _strlen_recursion(char *s);
  */
 int factorial(int n);
 
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: param1 
+ * @y: param2
+ * Return: int
+ */
+int _pow_recursion(int x, int y);
+
 #endif
