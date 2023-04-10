3c1dd0f5c22a3009ed2dd567cf54d2d96e5d24f3
Zibxto <christianaziba@gmail.com>
2023-04-10 15:27:27 +0100

    added file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ * @argc: number of arguments passed to program
+ * @argv: array of arguments passed to program
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int i;
+
+	int mul = 1;
+
+	if (argc == 1)
+	{
+		printf("%s\n", "Error");
+	}
+
+
+	for (i = 1; i < argc; i++)
+	{
+
+		mul *= atoi(argv[i]);
+	}
+
+		printf("%d\n", mul);
+	return (0);
+
+
+
+
+
+}
