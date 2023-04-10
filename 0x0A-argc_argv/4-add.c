cb96fff4d7fe4e28185535f7da0121befe22b9c4
Zibxto <christianaziba@gmail.com>
2023-04-10 17:03:55 +0100

    added file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+  * main - Entry point
+  * @argc: number of arguments passed to program
+  * @argv: array of arguments passed to program
+  * Return: Always 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+
+			if (isdigit(*argv[i]) == 0)
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+
+	}
+
+	return (0);
+
+}
