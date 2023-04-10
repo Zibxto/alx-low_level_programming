6ed47afb291b791a665f94a0bca8febd0ab967f9
Zibxto <christianaziba@gmail.com>
2023-04-10 14:20:17 +0100

    added file

---
+++ 1-args.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments passed to program
+ * @argv: array of arguments passed to program
+ * Return: Always 0
+ */
+
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+
+}
