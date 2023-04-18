e5c0a9fadae4142ac2a29e4b6ae21bd259192ead
Zibxto <christianaziba@gmail.com>
2023-04-18 14:19:58 +0100

    added file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - creates a dog struct
+ * @name: member 1
+ * @age: member 2
+ * @owner: member 3
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
