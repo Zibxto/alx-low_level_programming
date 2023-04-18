f8b1c2e15752881716431683cf09231def7fae48
Zibxto <christianaziba@gmail.com>
2023-04-18 14:50:49 +0100

    checked if d initiated correctly

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
  * init_dog - initializes a variable  of type struct dog
@@ -11,6 +12,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+
+	if (d == NULL)
+		exit (1);
 
 	d->name = name;
 	d->age = age;
