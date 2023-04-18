26461f3cee15fd6716d15afe25fb26132048ba64
Zibxto <christianaziba@gmail.com>
2023-04-18 15:47:38 +0100

    tracked elements with null value

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,6 +10,9 @@
 void print_dog(struct dog *d)
 {
 
+	if (d == NULL)
+		return;
+
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
@@ -21,8 +24,5 @@ void print_dog(struct dog *d)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
-
-	if (d == NULL)
-		return;
 
 }
