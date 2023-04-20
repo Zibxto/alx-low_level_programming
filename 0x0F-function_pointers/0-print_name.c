24a5bc0c08683eace10f6d422133983a9b56d61b
Zibxto <christianaziba@gmail.com>
2023-04-20 15:08:47 +0100

    added file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+	if (name != NULL && f != NULL)
+		f(name);
+
+
+}
