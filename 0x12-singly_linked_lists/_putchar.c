d9c64daa8d9d7cb2ecbf369098113172badfc349
zibxto <christianaziba@gmail.com>
2023-05-01 15:07:26 +0100

    included betty style

--- 0x12-singly_linked_lists/_putchar.c
+++ 0x12-singly_linked_lists/_putchar.c
@@ -1,6 +1,12 @@
 #include <unistd.h>
 
+/**
+ * _putchar - outputs a character to stdout
+ * @character: character value
+ * Return: int
+ */
+
 int _putchar(char *character)
 {
-	return(write(1, &character, 1));
+	return (write(1, &character, 1));
 }
