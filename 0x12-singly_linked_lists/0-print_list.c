77ca0633d541d9c04bdde5ead685da636336176a
zibxto <christianaziba@gmail.com>
2023-05-01 15:10:28 +0100

    included the conditional statement in the while loop

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,13 +11,16 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
 
 	while (h != NULL)
 	{
-		count++;
+	
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
 		printf("%d %s\n", h->len, h->str);
+
+		count++;
 		h = h->next;
 	}
 
