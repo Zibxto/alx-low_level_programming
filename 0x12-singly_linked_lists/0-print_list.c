33f484b1976daa3913009b0534fddbb66e8cdc24
zibxto <christianaziba@gmail.com>
2023-05-01 15:01:21 +0100

    added file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer parameter
+ * Return: number of nodes
+ */
+
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+
+	while (h != NULL)
+	{
+		count++;
+		printf("%d %s\n", h->len, h->str);
+		h = h->next;
+	}
+
+	return (count);
+}
