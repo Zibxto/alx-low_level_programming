48d0093d306b905553ac4c31d45e7d2b56bd0bbe
zibxto <christianaziba@gmail.com>
2023-05-02 16:07:21 +0100

    added file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: pointer to the head pointer
+ */
+
+void free_listint2(listint_t **head)
+{
+
+	listint_t *ptr;
+
+	if (*head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		ptr = *head;
+		*head = ptr->next;
+		free(ptr);
+	}
+
+
+}
