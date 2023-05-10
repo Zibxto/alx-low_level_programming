1a839a057649794023f8d1988b49207e23ad8b9b
zibxto <christianaziba@gmail.com>
2023-05-10 14:47:13 +0100

    added file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,4 +15,8 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
+int __exit(int err, char *s, int fd);
+
+
+
 #endif
