4549f6caba08636fcbe4b2e62b39cd1d8abc36cd
zibxto <christianaziba@gmail.com>
2023-07-11 01:05:42 +0100

    updated file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,9 +20,6 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
@@ -42,7 +39,7 @@ int create_file(const char *filename, char *text_content)
 		len++;
 	}
 
-	n = write(fd, &text_content, len);
+	n = write(fd, text_content, len);
 	if (n == -1)
 		return (-1);
 
