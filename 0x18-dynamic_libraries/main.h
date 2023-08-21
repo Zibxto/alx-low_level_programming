13146ecda2ec55194bc46a81e48dcf8b0c20e1c0
zibxto <christianaziba@gmail.com>
2023-08-21 17:12:36 +0100

    added prototypes for add and factorial functions

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -23,6 +23,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-
+int add(int a, int b);
+int factorial(int n);
 #endif
